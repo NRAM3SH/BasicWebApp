98d6fbb8dfc00b8e36ce741eec738c046c5abc8f
Neeraj Ramesh <neerajr@LAPTOP-0HDHF74P.localdomain>
2022-09-22 03:49:21 -0400

    Added new query case for porsche

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("porsche")) {
+            return "Porsche is a German automobile manufacturer. The specialize in developing " +
+                    "high performance vehicles, from track machines like the 911 GT3 RS," + 
+                    "hypercars like the 918, and even electric cars like the Taycan." ;
+
         }
         return "";
     }
