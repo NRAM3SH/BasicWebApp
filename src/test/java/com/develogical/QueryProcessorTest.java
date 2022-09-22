b1357b6470f9d277ddd2c990d79ad3d465da0949
Neeraj Ramesh <neerajr@LAPTOP-0HDHF74P.localdomain>
2022-09-22 10:39:12 -0400

    Fixed what is your name

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -27,7 +27,7 @@ public class QueryProcessorTest {
 
     @Test
     public void knowsTeamName() throws Exception {
-        assertThat(queryProcessor.process("what is your team name"), containsString("Neeraj"));
+        assertThat(queryProcessor.process("what is your name"), containsString("Neeraj"));
     }
 
 }
