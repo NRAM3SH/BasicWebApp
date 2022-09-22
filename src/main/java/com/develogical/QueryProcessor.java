2be7a822edfc961c4149dbf8bcb2e9a8aa59fe65
Neeraj Ramesh <neerajr@LAPTOP-0HDHF74P.localdomain>
2022-09-22 03:54:44 -0400

    Fixed spacing in porsche query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,10 +9,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.toLowerCase().contains("porsche")) {
             return "Porsche is a German automobile manufacturer. The specialize in developing " +
-                    "high performance vehicles, from track machines like the 911 GT3 RS," + 
+                    "high performance vehicles, from track machines like the 911 GT3 RS, " + 
                     "hypercars like the 918, and even electric cars like the Taycan." ;
 
-        //}
+        }
         return "";
     }
 }
